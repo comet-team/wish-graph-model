b1332337466536abb4d3b1ab9ca94a4da61821fa
Ulyana Klyuchnikova <udklyuchnikova@edu.hse.ru>
2022-04-28 19:53:31 +0300

    + different request answer

--- model/graph_model.py
+++ model/graph_model.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from scipy.spatial.distance import squareform, pdist
 
 class ModelControl:
     def get_owner_recommendations(self, owner, connection_df):
@@ -7,11 +8,15 @@ class ModelControl:
         connection_df = connection_df.pivot(index='user_id', columns='creator_id', values=['total_value'])
         connection_df = connection_df.fillna(value=0)
         table = []
+        owner_line = np.array(connection_df.loc[owner, 'total_value'])
         for id in connection_df.index:
-         table.append(np.array(connection_df.loc[id, 'total_value']))
-        table = np.array(table)
-        line = np.array(connection_df.loc[owner, 'total_value'])
-        recommendation_weights = pd.DataFrame({'creator_id': connection_df.columns.get_level_values('creator_id'), 'rate': np.matmul(np.dot(line, table.T), table)})
+            pair = np.array([np.array(connection_df.loc[id, 'total_value']), owner_line])
+            table.append(pdist(pair, 'sqeuclidean'))
+        table = np.array(table).T
+        table = 1 - table / np.sqrt(table.std())
+        table = np.squeeze(table)
+        recommendation_weights = pd.DataFrame({'creator_id': connection_df.columns.get_level_values('creator_id'), 'rate': np.matmul(table, connection_df.loc[:, "total_value"])})
+
         recommendation_weights.sort_values(by = 'rate', ignore_index=True, inplace=True)
         return recommendation_weights['creator_id']
 
